f1938c97080fd99aefb0f89aeaa7b3a3c0bd6a9f
dihernandez <dianah13@mit.edu>
2015-06-09 14:56:43 -0400

    initial commit has hellow world and the beginnings of a scheduler, just blinks 3 leds in a row right now, see pictures for breadboard layout and pin layout

---
+++ strong_priority.c
@@ -0,0 +1,89 @@
+/* This is an attempt to implement a hard priority system for interacting with a microcontroller
+ * learning about using interrupts with atmega328p (arduino uno)
+ * */
+#include <stdbool.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+short RED_PIN, YELLOW_PIN, GREEN_PIN, BUZZER_PIN;
+const int DELAY_TIME = 300;
+bool interrupted;
+
+enum LEDS {RED,YELLOW,GREEN};
+void setup(){
+    interrupted = false;
+    RED_PIN = PB3; YELLOW_PIN = PB2; GREEN_PIN = PB1; BUZZER_PIN = PB4;
+    // set thee following ports to output
+    DDRB |= _BV(RED_PIN);
+    DDRB |= _BV(YELLOW_PIN); 
+    DDRB |= _BV(GREEN_PIN); 
+    DDRB |= _BV(BUZZER_PIN);
+    //set pot as input
+    //PRB &= _BV(DDB2); //pot pin
+}
+
+void flipSwitch() {
+    //read pot input
+    //if potInput > 2.5V
+    //  interrupt
+}
+
+void soundBuzzer() {
+    
+};
+
+
+void blinkRedLED() {
+    PORTB |= _BV(PORTB3);
+    _delay_ms(DELAY_TIME);
+    PORTB &= ~_BV(PORTB3);
+    _delay_ms(DELAY_TIME);
+}
+
+void blinkYelloLED() {
+    PORTB |= _BV(PORTB2);
+    _delay_ms(DELAY_TIME);
+    PORTB &= ~_BV(PORTB2);
+    _delay_ms(DELAY_TIME);
+
+
+}
+
+void  blinkGreenLED() {
+    PORTB |= _BV(PORTB1);
+    _delay_ms(DELAY_TIME);
+    PORTB &= ~_BV(PORTB1);
+    _delay_ms(DELAY_TIME);
+}
+
+void blinkLED(enum LEDS led){
+    switch (led){
+        case RED:
+            blinkRedLED();
+            break;
+        case YELLOW:
+            blinkYelloLED();
+            break;
+        case GREEN:
+            blinkGreenLED();
+            break;
+    }
+    PORTB |= _BV(PORTB5);
+}
+
+
+void runScheduler(){
+    while(!interrupted) {
+        blinkLED(RED);
+        blinkLED(YELLOW);
+        blinkLED(GREEN);
+    }
+    soundBuzzer();
+    _delay_ms(DELAY_TIME);
+}
+
+int main(){
+    setup();
+    runScheduler();
+    return 0;
+}
